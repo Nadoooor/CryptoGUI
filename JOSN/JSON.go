cbf101dff5dd7490da3d99d262a985e5d765bb75
Nader Sayed <nadersayed742@gmail.com>
2025-09-12 03:26:44 +0300

    First working version.

---
+++ JOSN/JSON.go
@@ -0,0 +1,28 @@
+package JOSN
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type History struct {
+	DateNtime string `json:"TIMESTAMP"`
+	INPUT     string `json:"INPUT"`
+	OUTPUT    string `json:"OUTPUT"`
+	CIPHER    string `json:"CIPHER"`
+}
+
+func Save(things []History) {
+
+	data, _ := json.MarshalIndent(things, "", "  ")
+	os.WriteFile("history.json", data, 0644)
+}
+
+func Load() []History {
+	here, _ := os.ReadFile("history.json")
+	var history []History
+	json.Unmarshal(here, &history)
+
+	return history
+
+}
--- Main/Main.go
+++ Main/Main.go
@@ -4,6 +4,9 @@ import (
 	"CryptoGUI/Base"
 	"CryptoGUI/Binary"
 	"CryptoGUI/Hex"
+	"CryptoGUI/JOSN"
+
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -16,6 +19,8 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("CryptoGUI")
 	current := "blank"
+	historyload := JOSN.Load()
+
 	background, _ := fyne.LoadResourceFromPath("CryptoGUI.png")
 	back := canvas.NewImageFromResource(background)
 	back.FillMode = canvas.ImageFillStretch
@@ -26,18 +31,29 @@ func main() {
 	in.Resize(fyne.NewSize(232, 40))
 
 	view := widget.NewMultiLineEntry()
-	//view.Disable()
+
 	view.Move(fyne.NewPos(35, 275))
 	view.Resize(fyne.NewSize(232, 175))
-	ciphers := []string{"Base64", "Base32", "Hex", "Binary", "Caeser"}
+	ciphers := []string{"Base64", "Base32", "Hex", "Binary"}
 	drop := widget.NewSelect(ciphers, func(s string) {
 		current = s
+
 	})
 	drop.PlaceHolder = "Select Cipher"
 	drop.Move(fyne.NewPos(35, 190))
 	drop.Resize(fyne.NewSize(229, 40))
 	view.Wrapping = fyne.TextWrapWord
 	view.SetMinRowsVisible(3)
+	History := container.NewVBox()
+	INPUT := widget.NewMultiLineEntry()
+	INPUT.Wrapping = fyne.TextWrapWord
+	INPUT.SetMinRowsVisible(3)
+	OUTPUT := widget.NewMultiLineEntry()
+	OUTPUT.Wrapping = fyne.TextWrapWord
+	OUTPUT.SetMinRowsVisible(3)
+	CIPHER := widget.NewMultiLineEntry()
+	CIPHER.Wrapping = fyne.TextWrapWord
+	CIPHER.SetMinRowsVisible(3)
 	decode := widget.NewButton("Decode", func() {
 		switch current {
 
@@ -52,6 +68,28 @@ func main() {
 		default:
 			view.SetText("Choose a Chipher!!")
 		}
+		if in.Text != "" && view.Text != "Choose a Chipher!!" {
+			time := time.Now().Format("2006-01-02 PM 15:04:05")
+			first := in.Text
+			second := view.Text
+			Third := current
+			History.Add(widget.NewButton(time, func() {
+				INPUT.SetText(first)
+				OUTPUT.SetText(second)
+				CIPHER.SetText(Third)
+			}))
+			History.Refresh()
+			newrow := JOSN.History{
+				DateNtime: time,
+				INPUT:     in.Text,
+				OUTPUT:    view.Text,
+				CIPHER:    current,
+			}
+			historyload = append(historyload, newrow)
+			JOSN.Save(historyload)
+
+		}
+
 	})
 	decode.Resize(fyne.NewSize(95, 47))
 	decode.Move(fyne.NewPos(35, 495))
@@ -69,10 +107,38 @@ func main() {
 		default:
 			view.SetText("Choose a Chipher!!")
 		}
+		if in.Text != "" && view.Text != "Choose a Chipher!!" {
+			time := time.Now().Format("2006-01-02 PM 15:04:05")
+			first := in.Text
+			second := view.Text
+			Third := current
+			History.Add(widget.NewButton(time, func() {
+				INPUT.SetText(first)
+				OUTPUT.SetText(second)
+				CIPHER.SetText(Third)
+			}))
+			History.Refresh()
+			newrow := JOSN.History{
+				DateNtime: time,
+				INPUT:     in.Text,
+				OUTPUT:    view.Text,
+				CIPHER:    current,
+			}
+			historyload = append(historyload, newrow)
+			JOSN.Save(historyload)
+		}
 	})
 	encode.Resize(fyne.NewSize(95, 47))
 	encode.Move(fyne.NewPos(170, 495))
+	for _, Each := range historyload {
+		History.Add(widget.NewButton(Each.DateNtime, func() {
+			INPUT.SetText(Each.INPUT)
+			OUTPUT.SetText(Each.OUTPUT)
+			CIPHER.SetText(Each.CIPHER)
+		}))
 
+	}
+	History.Refresh()
 	container1 := container.NewWithoutLayout(back, in, view, drop, decode, encode)
 
 	background2, _ := fyne.LoadResourceFromPath("CryptoGUIHistory.png")
@@ -80,14 +146,25 @@ func main() {
 	back2.FillMode = canvas.ImageFillStretch
 	back2.Resize(fyne.NewSize(300, 600))
 
-	container2 := container.NewWithoutLayout(back2)
+	scroll := container.NewScroll(History)
+
+	scroll.Move(fyne.NewPos(35, 105))
+	scroll.Resize(fyne.NewSize(230, 120))
+	INPUT.Move(fyne.NewPos(35, 275))
+	INPUT.Resize(fyne.NewSize(231, 70))
+	OUTPUT.Move(fyne.NewPos(35, 380))
+	OUTPUT.Resize(fyne.NewSize(231, 67))
+	CIPHER.Move(fyne.NewPos(35, 485))
+	CIPHER.Resize(fyne.NewSize(231, 67))
+
+	container2 := container.NewWithoutLayout(back2, scroll, INPUT, OUTPUT, CIPHER)
 	tabs := container.NewAppTabs(
 		container.NewTabItem("Tool", container1),
 		container.NewTabItem("History", container2),
 	)
 	w.SetContent(tabs)
 	w.SetFixedSize(true)
-	w.Resize(fyne.NewSize(300, 600))
+	w.Resize(fyne.NewSize(300, 630))
 
 	w.ShowAndRun()
 }
